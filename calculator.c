e94290017b9b51b424398f8b3764a2282600c259
sss-sai <54887351+sss-sai@users.noreply.github.com>
2019-09-04 14:49:20 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,30 +1,36 @@
 
-# include <stdio.h>
-int main() {
-    char operator;
-    double firstNumber,secondNumber;
-    printf("Enter an operator (+, -, *,): ");
-    scanf("%c", &operator);
-    printf("Enter two operands: ");
-    scanf("%lf %lf",&firstNumber, &secondNumber);
-    switch(operator)
-    {
-        case '+':
-            printf("%.1lf + %.1lf = %.1lf",firstNumber, secondNumber, firstNumber + secondNumber);
-            break;
-        case '-':
-            printf("%.1lf - %.1lf = %.1lf",firstNumber, secondNumber, firstNumber - secondNumber);
-            break;
-        case '*':
-            printf("%.1lf * %.1lf = %.1lf",firstNumber, secondNumber, firstNumber * secondNumber);
-            break;
-        case '/':
-            printf("%.1lf / %.1lf = %.1lf",firstNumber, secondNumber, firstNumber / secondNumber);
-            break;
-        // operator doesn't match any case constant (+, -, *, /)
-        default:
-            printf("Error! operator is not correct");
-    }
-    
-    return 0;
+#include <stdio.h>
+ 
+int main()
+{
+	char Operator;
+	float num1, num2, result = 0;
+	
+	printf("\n Please Enter an Operator (+, -, *, /)  :  ");
+  	scanf("%c", &Operator);
+  	
+	printf("\n Please Enter the Values for two Operands: num1 and num2  :  ");
+  	scanf("%f%f", &num1, &num2);
+  	
+  	switch(Operator)
+  	{
+  		case '+':
+  			result = num1 + num2;
+  			break;
+  		case '-':
+  			result = num1 - num2;
+  			break;  			
+  		case '*':
+  			result = num1 * num2;
+  			break;
+  		case '/':
+  			result = num1 / num2;
+  			break;
+		default:
+			printf("\n You have enetered an Invalid Operator ");				    			
+	}
+  
+	printf("\n The result of %.2f %c %.2f  = %.2f", num1, Operator, num2, result);
+	
+  	return 0;
 }
